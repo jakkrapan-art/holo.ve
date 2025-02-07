9a67f561d12df23fc06b5a6983fbaee3dcfaac26
Jakkrapan Thongphet <jakkrapan.art@gmail.com>
2025-02-08 03:19:20 +0700

    update calculate enemy movement using enemy move speed data

--- scripts/entity/enemy/enemy.gd
+++ scripts/entity/enemy/enemy.gd
@@ -10,7 +10,9 @@ var maxHp: int = 0;
 var currentHp: int = 0;
 var armor: int = 0;
 var mArmor: int = 0;
-var moveSpeed: int = 0;
+var moveSpeed: float = 0;
+
+var spawnTime = 0
 
 func setup(hp: int, armor: int, mArmor: int, moveSpeed: int, texture: Texture2D):
 	setTexture(texture);
@@ -18,7 +20,13 @@ func setup(hp: int, armor: int, mArmor: int, moveSpeed: int, texture: Texture2D)
 	currentHp = hp;
 	self.armor = armor;
 	self.mArmor = mArmor;
-	self.moveSpeed = moveSpeed;
+	self.moveSpeed = calculate_pathfollow_speed(get_parent() as Path2D, moveSpeed);
+	spawnTime = Time.get_ticks_msec();
+	
+	connect("onReachEndPoint", Callable(self, "showLifeTime"));
+
+func showLifeTime():
+	print("lifetime: ", (Time.get_ticks_msec() - spawnTime)/1000);
 
 func _process(_delta):
 	if(progress_ratio == 1):
@@ -26,7 +34,7 @@ func _process(_delta):
 		queue_free()
 
 func _physics_process(delta):
-	progress_ratio += 0.1 * delta
+	progress_ratio += moveSpeed * delta;
 
 func setTexture(image: Texture2D):
 	if(sprite != null && image != null):
@@ -41,6 +49,16 @@ func recvDamage(damage: int) -> int:
 func dead():
 	onDead.emit();
 	queue_free();
+	
+func calculate_pathfollow_speed(path: Path2D, moveSpeed: float) -> float:
+	var curve = path.curve
+	if curve.point_count < 2:
+		return 0.0  # Not enough points to move
+	
+	var pointCount = curve.point_count - 2; # remove start and end node
+	var totalSegments = pointCount - 1
+	return 1.0 / (totalSegments * moveSpeed)
+
 
 signal onReachEndPoint();
 signal onDead();

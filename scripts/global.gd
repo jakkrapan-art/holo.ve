7c599d82630148e3888aa075c6cd2e5fdafd8782
Metawat Tuntaisong <methawat.ts@gmail.com>
2025-05-18 17:53:22 +0700

    parse selected deck data to game scene (WIP)
    use temporary data for easy implement

--- scripts/global.gd
+++ scripts/global.gd
@@ -1,3 +1,12 @@
 extends Node
 
-var selected_deck: String = ""
+#var selected_deck: String = ""
+var selected_deck: String = "Myth" #temporary
+var selected_data_file: String = "towers_Myth.yaml" #temporary
+#might as well remember all this
+#Myth:
+  #name: Myth
+  #sprite: Myth.png
+  #architype: Hololive
+  #data_file: towers_Myth.yaml
+  #branch: en

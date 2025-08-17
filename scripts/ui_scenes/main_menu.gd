562f5f44ba1ee3e799b29c79bf1f7f6f8b13ed92
Metawat Tuntaisong <methawat.ts@gmail.com>
2025-08-17 17:33:27 +0700

    add main menu graphic

--- scripts/ui_scenes/main_menu.gd
+++ scripts/ui_scenes/main_menu.gd
@@ -2,11 +2,24 @@ extends Control
 
 func _ready():
 	ResourceManager.loadResources();
+	var btn_normal_pic = load("res://resources/ui_component/ui_asset/button_off.png")
+	var btn_press_pic = load("res://resources/ui_component/ui_asset/button_on.png")
 
 	# Connect button signals
-	get_node("VBoxContainer/StartButton").pressed.connect(_on_start_pressed)
-	get_node("VBoxContainer/OptionsButton").pressed.connect(_on_options_pressed)
-	get_node("VBoxContainer/ExitButton").pressed.connect(_on_exit_pressed)
+	var start = get_node("VBoxContainer/StartButton")
+	start.pressed.connect(_on_start_pressed)
+	var option = get_node("VBoxContainer/OptionsButton")
+	option.pressed.connect(_on_options_pressed)
+	var exit = get_node("VBoxContainer/ExitButton")
+	exit.pressed.connect(_on_exit_pressed)
+	
+	_set_btn_image(start, btn_normal_pic, btn_press_pic)
+	_set_btn_image(option, btn_normal_pic, btn_press_pic)
+	_set_btn_image(exit, btn_normal_pic, btn_press_pic)
+	
+func _set_btn_image(btn, normal, press):
+	btn.texture_normal = normal
+	btn.texture_pressed = press
 
 func _on_start_pressed():
 	#get_tree().change_scene_to_file("res://scenes/tower_select_tmp_scene.tscn")  # Change to your actual game scene

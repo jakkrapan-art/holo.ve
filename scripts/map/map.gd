311ffc2f3dd84f66b82294b61c08635961c3e0b7
Jakkrapan Thongphet <jakkrapan.art@gmail.com>
2026-01-25 13:03:26 +0700

    refactor: define static map size and starting coordinates in Map class

--- scripts/map/map.gd
+++ scripts/map/map.gd
@@ -1,23 +1,28 @@
 extends TileMap
 class_name Map
 
-@export var mapSize: Vector2;
+static var mapSize: Vector2 = Vector2(11, 10);
+static var startX: int = 8;
+static var startY: int = 3;
+
 @export var path: Path2D;
 @onready var path_bake: PathBakeAuto = $Path2D
 static var availableCells: Array[Vector2i];
 
 static func isCellAvailable(cellPos: Vector2i) -> bool:
+	if(cellPos.x < startX || cellPos.y < startY || cellPos.x > startX + mapSize.x || cellPos.y > startY + mapSize.y):
+		return false;
 	return availableCells.has(cellPos);
-	
+
 func removeAvailableCell(cell: Vector2i):
 	var index = availableCells.find(cell);
 	if(index > -1):
 		availableCells.remove_at(index);
-		
+
 func addAvailableCell(cell: Vector2i):
 	if(availableCells.has(cell)):
 		return;
-		
+
 	availableCells.append(cell);
 
 func setup():

b76211aef3d30ba5cee029538b6fa0b53d1cb802
Jakkrapan Thongphet <jakkrapan.art@gmail.com>
2026-03-04 00:35:47 +0700

    remove .png extension from enemy loadinng

--- scripts/sprite_loader.gd
+++ scripts/sprite_loader.gd
@@ -30,7 +30,7 @@ static func preloadEnemy(mapName: String, types: Array[String]) -> void:
 
 	for type in types:
 		for enemy in enemyNames:
-			var full_path := "%s/%s/%s/%s/%s.png" % [
+			var full_path := "%s/%s/%s/%s/%s" % [
 				enemyPrefix,
 				mapName,
 				type,

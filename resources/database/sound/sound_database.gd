ba8183c8695ebe15d7c2a2793fd6b60067141279
Andoi <disanuwat.mp@gmail.com>
2026-04-09 21:52:34 +0700

    change debut voice

--- resources/database/sound/sound_database.gd
+++ resources/database/sound/sound_database.gd
@@ -53,15 +53,15 @@ static var sfx: Dictionary = { #สิ่งนั้นคือไฟล์ไ
 }
 
 static var voice: Dictionary = { #ตอนนี้ยังไม่เปิดใช้นะ แต่ใส่เผื่อมาก่อน //เสียงเปิดตัว (ขอเป็น debut_voice)
-	VOICE_NAME.debut_syrios: "gura/greeting_gura(keep).mp3",
-	VOICE_NAME.debut_hakka: "gura/greeting_gura(keep).mp3",
-	VOICE_NAME.debut_bettel: "gura/greeting_gura(keep).mp3",
+	VOICE_NAME.debut_syrios: "axel_Syrios/greeting_axel.mp3",
+	VOICE_NAME.debut_hakka: "hakkito/greeting_Banzoin_Hakka.mp3",
+	VOICE_NAME.debut_bettel: "gavis_bettel/greeting_gavis.mp3",
 	VOICE_NAME.debut_gura: "gura/greeting_gura(keep).mp3",
-	VOICE_NAME.debut_shinri: "gura/greeting_gura(keep).mp3",
-	VOICE_NAME.debut_flayon: "gura/greeting_gura(keep).mp3",
-	VOICE_NAME.debut_caliope: "gura/greeting_gura(keep).mp3",
-	VOICE_NAME.debut_ina: "gura/greeting_gura(keep).mp3",
-	VOICE_NAME.debut_altare: "gura/greeting_gura(keep).mp3",
-	VOICE_NAME.debut_kiara: "gura/greeting_gura(keep).mp3",
-	VOICE_NAME.debut_amelia: "gura/greeting_gura(keep).mp3"
+	VOICE_NAME.debut_shinri: "josuiji_shinri/greeting_josuiji.mp3",
+	VOICE_NAME.debut_flayon: "machina_x_flayon/greeting_flayon.mp3",
+	VOICE_NAME.debut_caliope: "calliope/greeting_Colliope_MinnaSannn.mp3",
+	VOICE_NAME.debut_ina: "ina/greeting_ina.mp3",
+	VOICE_NAME.debut_altare: "regis_altare/greeting_regis.mp3",
+	VOICE_NAME.debut_kiara: "kiara/greeting_Takanashi_Kiara.mp3",
+	VOICE_NAME.debut_amelia: "ame/greeting_ame_hello(keep).mp3"
 }
